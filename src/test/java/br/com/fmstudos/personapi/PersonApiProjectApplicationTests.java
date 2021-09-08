9af646c31b0deaed739ce9ea9e72b3234e879ae7
Flavio Medeiros <flaviotibra@gmail.com>
2021-09-08 10:18:32 -0300

    first commit

---
+++ src/test/java/br/com/fmstudos/personapi/PersonApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fmstudos.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
